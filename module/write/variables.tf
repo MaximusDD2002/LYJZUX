25e5fc7159c45e0a27a843c88783152437206af9
MaximusDD2002 <94798896+MaximusDD2002@users.noreply.github.com>
2023-05-19 00:49:05 +0300

    Create variables.tf

---
+++ module/write/variables.tf
@@ -0,0 +1,19 @@
+variable "answer_1" {
+  description = "Answer to question 1"
+}
+
+variable "answer_2" {
+  description = "Answer to question 2"
+}
+
+variable "answer_3" {
+  description = "Answer to question 3"
+}
+
+variable "answer_4" {
+  description = "Answer to question 4"
+}
+
+variable "answer_5" {
+  description = "Answer to question 5"
+}
