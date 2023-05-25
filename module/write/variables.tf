0e2d8906c82f2395c35b32df260f01886f1204ee
MaximusDD2002 <94798896+MaximusDD2002@users.noreply.github.com>
2023-05-25 15:48:07 +0300

    Update variables.tf

--- module/write/variables.tf
+++ module/write/variables.tf
@@ -1,19 +1,20 @@
 variable "answer_1" {
-  description = "Answer to question 1"
+    description = "Answer for the first question"
+    type = string
 }
-
 variable "answer_2" {
-  description = "Answer to question 2"
+    description = "Answer for the second question"
+    type = string
 }
-
 variable "answer_3" {
-  description = "Answer to question 3"
+    description = "Answer for the third question"
+    type = string
 }
-
 variable "answer_4" {
-  description = "Answer to question 4"
+    description = "Answer for the fourth question"
+    type = string
 }
-
 variable "answer_5" {
-  description = "Answer to question 5"
+    description = "Answer for the fifth question"
+    type = string
 }
